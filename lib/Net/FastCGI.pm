b918e28fdb7edf03422e082dfdab6b1f5bac9316
chansen <chansen@cpan.org>
2010-04-03 22:34:14 +0200

    Initial import of Net-FastCGI

---
+++ lib/Net/FastCGI.pm
@@ -0,0 +1,12 @@
+package Net::FastCGI;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.10';
+
+use Net::FastCGI::Constant;
+use Net::FastCGI::Protocol;
+
+1;
+
---
+++ t/lib/myconfig.pm
@@ -0,0 +1,9 @@
+package myconfig;
+
+use strict;
+
+BEGIN {
+    $ENV{NET_FASTCGI_PP} = 1;
+}
+
+1;
